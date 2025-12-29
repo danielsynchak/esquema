a745b587ed0fc317184a201fd1d367d2b66a63c5
masterpt06 <pmfrazaofidalgo@gmail.com>
2025-12-29 16:49:31 +0000

    Cenario

--- main.js
+++ main.js
@@ -66,7 +66,7 @@ let baseMesh;
 let carregador = new GLTFLoader();
 
 
-carregador.load('blender/RecordPlayerTeste1.gltf', function (gltf) {
+carregador.load('Cenario.gltf', function (gltf) {
     cena.add(gltf.scene);
 
     // encontrar a mesh pelo nome
